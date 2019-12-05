b47c0980fcebdf5ff478b5e139cc0a1cb98f0d0d
suzl <972509224@qq.com>
2019-12-06 01:48:46 +0800

    Initial commit

---
+++ structandfile/FileTest.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+)
+
+func main() {
+    // copy file 
+    file, err := os.Open("./structandfile/test.xxx")
+    if err != nil {
+        fmt.Println("open error", err)
+        return
+    }
+    defer file.Close()
+    // create file
+    copyFile, err := os.Create("./fileCopy.txt")
+
+    if err != nil {
+        fmt.Println("error", err)
+        return
+    }
+    defer copyFile.Close()
+    // read => to buffer
+    buffer := make([]byte, 4096)
+
+    for {
+        read, err := file.Read(buffer)
+        if err != nil && err == io.EOF {
+            fmt.Println("all done")
+            return
+        }
+        copyFile.Write(buffer[:read])
+    }
+}
+
+// file read and write
+// read by bytes and
