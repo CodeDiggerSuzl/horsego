7981419876b6e621826508674dfcd6ea16e8f4fe
suzl <joexsue@gmail.com>
2020-03-07 17:23:11 +0800

    :seedling: add code
    add code

---
+++ src/http/client_test.go
@@ -0,0 +1,26 @@
+package http
+
+import (
+    "fmt"
+    "net/http"
+    "testing"
+)
+
+func Test_Client(t *testing.T) {
+    resp, err := http.Get("http://www.baidu.com")
+    HandleErr(err)
+    defer resp.Body.Close()
+
+    buf := make([]byte, 4096)
+    var result string
+    for {
+        n, _ := resp.Body.Read(buf)
+        if n == 0 {
+            fmt.Println("Body.Read err:", err)
+            break
+        }
+        if n != 0 {
+            result += result
+        }
+    }
+}
