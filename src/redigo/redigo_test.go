1ed59c83cd013cc49f6d612e95ed9ffa0f8835f9
suzl <joexsue@gmail.com>
2020-03-07 19:39:55 +0800

    :seedling: [redis] test redis
    [redis] test redis

---
+++ src/redigo/redigo_test.go
@@ -0,0 +1,17 @@
+package redigo
+
+import (
+    "fmt"
+    "github.com/gomodule/redigo/redis"
+    "testing"
+)
+
+func Test_redigo(t *testing.T) {
+    conn, err := redis.Dial("tcp", "127.0.0.1:6379")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    res, _ := conn.Do("SET", "go", "redigo")
+    fmt.Println(res)
+}
